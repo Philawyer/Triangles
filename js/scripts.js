39d597ca47a8852d573da8ee96d4112509a25c86
philipM <phlpmwaura@yahoo.com>
2019-04-26 17:35:43 +0300

    First Commit

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+
+function trackTriangle() {
+    var Side1 = parseInt((document.getElementById)("Side1").value);
+    var Side2 = parseInt((document.getElementById)("Side2").value);
+    var Side3 = parseInt((document.getElementById)("Side3").value);
+
+    if (Side1 != Side2 && Side1 != Side3 && Side2 != Side3) {
+        alert("scalene");
+    } else {
+        alert("Error");
+    }
+};
+     if (Side3>= Side1 + Side2 || (Side3 == Side2 && Side2!= Side1) || (Side3!=Side2 && Side1==Side3) ||(Side1== Side2 && Side1!=Side3)== Side3 + Side1 || Side1 >= Side2 + Side3) {
+        document.getElementById("trackTriangle").innerHTML = "Not Triangle"};
+        alert(" Not triangle");
+  
+
+     elseif()
+
+    {if (Side1 == Side2 && Side2 == Side3);{
+    document.getdocumentById("trackTriangle").placeholder="Equilateral Triangle"
+}
+     elseif()
+ {if (Side3 == Side2 && Side2!= Side1) || (Side3!=Side2 && Side1==Side3) ||(Side1== Side2 && Side1!=Side3)
+    document.getdocumentById("trackTriangle").placeholder = "Isosceles Triangle"
+    }
+     elseif() 
+ {if (Side3!=Side2 && Side2!= Side1 && Side1!=Side3) {
+    document.getdocumentById("trackTriangle").placeholder = "Scalene Triangle"
+    }
